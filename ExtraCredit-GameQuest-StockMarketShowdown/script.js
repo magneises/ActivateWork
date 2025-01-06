d824a1329758d2ceac6061dd29b49484c55d0c5e
Preston G. <prestongilmore@gmail.com>
2025-01-06 09:00:21 -0700

    starting js

--- ExtraCredit-GameQuest-StockMarketShowdown/script.js
+++ ExtraCredit-GameQuest-StockMarketShowdown/script.js
@@ -1,1 +1,12 @@
 
+/* variable declarations */
+const priceOfStock = document.querySelector('.price-of-stock');
+const submitOrderBtn = document.querySelector('.submit-order-btn');
+
+
+/* event listeners */ 
+document.querySelector('.submit-order-btn').addEventListener('click', function(){ alert('Order Placed'); });
+
+
+
+
