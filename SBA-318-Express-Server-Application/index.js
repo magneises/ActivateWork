8318d7adcdf2f4d79510592f6d6bb58017afe677
Preston G. <prestongilmore@gmail.com>
2025-01-27 18:51:53 -0700

    updating index.js

--- SBA-318-Express-Server-Application/index.js
+++ SBA-318-Express-Server-Application/index.js
@@ -1,5 +1,42 @@
+
+
 import express from 'express';
-import fetch from 'node-fetch';
+import axios from 'axios';
+import 'dotenv/config';
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+
+
+// Categories note: muscles, exercises, custom  workouts
+
+
+
+
+app.get('/api/work', async (req, res) => {
+    const muscleGroup = req.query.muscle || 'biceps'; // default 
+
+    const options = {
+        method: 'GET',
+        url: 'https://${process.env.RAPIDAPI_HOST}/search',
+        params: { Muscles: muscleGroup },
+        headers: {
+            'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+            'x-rapidapi-host': process.env.RAPIDAPI_HOST,
+        },
+    };
+
+    try {
+        const response = await axios.request(options);
+        res.json(response.data);
+    } catch(e) {
+        console.log('Error fetching data from Work Out API:', error.message)
+        res.status(500).json({ error: 'Failed to fetch workout data' })
+    }
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:' + PORT);
+})
